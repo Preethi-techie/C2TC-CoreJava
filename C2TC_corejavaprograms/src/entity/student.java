d9554ed5977fe3cfe676bc9d6e073a2f942cef1e
sunit <sunit@SUNITHADEVEN.mshome.net>
2025-06-13 12:30:00 +0530

    this program is based on to string methods.

--- C2TC_corejavaprograms/src/entity/student.java
+++ C2TC_corejavaprograms/src/entity/student.java
@@ -1,14 +1,36 @@
 package entity;
 
 public class student {
-	int id;
-	String name;
-	int age;
-	
-	public void show() {
-		System.out.println(id +":"+name+":"+age);
+	private int id;
+	private String name;
+	private int age;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "student [id=" + id + ", name=" + name + ", age=" + age + "]";
 	}
 	
 	}
+	
+
+	
+
 
 
--- C2TC_corejavaprograms/src/entity/studentDemo.java
+++ C2TC_corejavaprograms/src/entity/studentDemo.java
@@ -3,10 +3,17 @@ package entity;
 public class studentDemo {
 	public static void main(String args[]) {
 		student stu = new student();
-		stu.id=101;
-		stu.name="Abi";
-		stu.age=21;
-		stu.show();
+		stu.setId(101);
+		stu.setName("Abi");
+		stu.setAge(21);
+		System.out.println(stu);
+		
+		student stu1 = new student();
+		stu1.setId(102);
+		stu1.setName("Sunitha");
+		stu1.setAge(20);
+		System.out.println(stu1);
+		
 		
 	}
 
