62f92d1a1da5a6e3a417c520d1c86654149b1128
sunit <sunit@SUNITHADEVEN.mshome.net>
2025-06-13 11:45:33 +0530

    My new entity package

---
+++ C2TC_corejavaprograms/src/entity/studentDemo.java
@@ -0,0 +1,13 @@
+package entity;
+
+public class studentDemo {
+	public static void main(String args[]) {
+		student stu = new student();
+		stu.id=101;
+		stu.name="Abi";
+		stu.age=21;
+		stu.show();
+		
+	}
+
+}
