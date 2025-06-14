031fc2f8ee5ad884121c00b0419bb3348dcdb36d
sunit <sunit@SUNITHADEVEN>
2025-06-14 18:17:36 +0530

    If else statement program in java.

---
+++ C2TC_corejavaprograms/src/day2/IfelseJava.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class IfelseJava {
+
+	public static void main(String[] args) {
+		int age=11;
+		if(age>18)
+			System.out.println("Eligible for voting");
+		else
+			System.out.println("Not eligible for voting");
+
+	}
+
+}
