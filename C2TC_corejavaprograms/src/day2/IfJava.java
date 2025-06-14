de01e5acbd0502237339de8d8c6c32d860d8281e
sunit <sunit@SUNITHADEVEN>
2025-06-14 18:12:57 +0530

    Conditional if statement program in java.

---
+++ C2TC_corejavaprograms/src/day2/IfJava.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class IfJava {
+
+	public static void main(String[] args) {
+		int x=5;
+		if(x!=5)
+			System.out.println("value of x is not 5");
+		System.out.println("Value of x is 5");
+
+	}
+
+}
