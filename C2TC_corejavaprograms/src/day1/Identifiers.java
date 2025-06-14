ca72847505aaddf45032fe965b2bdc311269e394
sunit <sunit@SUNITHADEVEN>
2025-06-14 17:54:46 +0530

    identifire program in java.

---
+++ C2TC_corejavaprograms/src/day1/Identifiers.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Identifiers {
+
+	public static void main(String[] args) {
+		int $number=20;
+		System.out.println("Value of the number variable is :"+$number);
+		String studentName="Sunitha";
+		System.out.println("Value of studentName is :"+studentName);
+
+	}
+
+}
