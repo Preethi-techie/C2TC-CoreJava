c349cab7882da684c7347d286aae0fdcf818fe05
sunit <sunit@SUNITHADEVEN.mshome.net>
2025-06-16 13:38:04 +0530

    a customer entity class program

---
+++ C2TC_corejavaprograms/src/entity/customerDemo.java
@@ -0,0 +1,23 @@
+package entity;
+
+public class customerDemo {
+	public static void main(String[]args) {
+		customer cs=new customer();
+		cs.setCustomerId(101);
+		cs.setCustomerName("sunitha");
+		cs.setCity("chennai");
+		System.out.println(cs);
+		
+		customer cs1=new customer();
+		cs1.setCustomerId(102);
+		cs1.setCustomerName("sweetha");
+		cs1.setCity("chennai");
+		System.out.println(cs1);
+		
+		
+		
+		
+		
+	}
+
+}
