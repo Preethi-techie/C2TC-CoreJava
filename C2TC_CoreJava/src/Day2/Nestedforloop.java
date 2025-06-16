e2747302b10490511401c0d28d2eabdfe729b48f
Preethi-techie <preethisekar253@gmail.com>
2025-06-16 23:25:40 +0530

    contains nestedforloop

---
+++ C2TC_CoreJava/src/Day2/Nestedforloop.java
@@ -0,0 +1,14 @@
+package Day2;
+public class Nestedforloop{
+	public static void main(String[] args) {
+		// program to print multiplication
+		for (int i = 10; i <= 10; i++) {
+			for (int j = 1; j <= 10; j++) {
+				System.out.println(i + "*" + j + "= " + i * j);
+			}
+			System.out.println();
+
+		}
+
+	}
+}
