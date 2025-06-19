9b16746b09381e48c089900eac0a7884e5d95ae7
Preethi-techie <preethisekar253@gmail.com>
2025-06-19 23:15:51 +0530

    updated

--- C2TC_CoreJava/src/Day4singleinheritance/SLinheritance.java
+++ C2TC_CoreJava/src/Day4singleinheritance/SLinheritance.java
@@ -1,6 +1,6 @@
 package Day4singleinheritance;
 
-public class SLinheritance {
+public class SLinheritance{
 	public static void main(String[]args) {
    Trainer t1 = new Trainer(101,"preethi", "chennai", "student","agni college");
    System.out.println(t1);
